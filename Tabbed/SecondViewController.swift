cde4b68223c13f40709397bdda3f836a39541e5e
DAM <sergiodiaz808@gmail.com>
2016-12-15 18:39:55 +0100

    Initial Commit

---
+++ Tabbed/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Tabbed
+//
+//  Created by DAM on 15/12/16.
+//  Copyright © 2016 DAM. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
